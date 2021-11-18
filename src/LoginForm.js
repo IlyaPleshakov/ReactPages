0ad409f732544810adf5b7c356b4d4f9a8a48870
Ilya <ilyapl1994@mail.ru>
2021-11-18 17:15:48 +0300

    add fetch to login

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -17,9 +17,11 @@ export default class LoginForm extends React.Component {
         this.updateInputLogin = this.updateInputLogin.bind(this);
         this.updateInputPassword = this.updateInputPassword.bind(this);
         this.state = {
-            login: 'admin',
+            username: 'admin',
             password: 'admin'
         }
+        this.my='';
+
     }
     handleSubmit(event) {
         event.preventDefault()
@@ -27,17 +29,32 @@ export default class LoginForm extends React.Component {
     }
 
     async userLoginFetch() {
-        let url = 'https://jsonplaceholder.typicode.com/todos';
-        return fetch(url).then((res) => {
-            localStorage.setItem('id_token', res.token) // Store token 
-        })
+        let url = 'http://localhost:8080/login';
+        return fetch(url,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain;charset=UTF-8',
+                'application':'x-www-form-urlencoded',
+                'X-Content-Type-Options':'nosniff',
+                'X-XSS-Protection':'1; mode=block',
+                'Cache-Control':'no-cache, no-store, max-age=0, must-revalidate',
+                'Pragma':'no-cache',
+                'Keep-Alive':'timeout=60',
+                'Connection':'keep-alive',
+                'Origin':'http://localhost:3000/'
+              },
+              body: JSON.stringify(this.state),
+              credentials: 'include',
+              referrerPolicy: 'no-referrer'
+        }).then((res) => {
+            res.json()})
             .catch(err => {
                 console.log(err);
             });
     }
 
     updateInputLogin(event) {
-        this.setState({ login: event.target.value })
+        this.setState({ username: event.target.value })
     }
     updateInputPassword(event) {
         this.setState({ password: event.target.value })
@@ -47,10 +64,10 @@ export default class LoginForm extends React.Component {
             <div style={styleLogin}>
                 <form onSubmit={this.handleSubmit}>
                     Введите логин {this.props.authenticate}
-                    <input type="text" name="login" value={this.state.login} onChange={this.updateInputLogin} /><br />
+                    <input type="text" name="username" value={this.state.login} onChange={this.updateInputLogin} /><br />
                     Введите пароль
                     <input type="text" name="password" value={this.state.password} onChange={this.updateInputPassword} />
-                    <MyButton name="Войти" onClick={this.props.onClick} type="submit" />
+                    <MyButton name="Войти" onClick={this.props.userLoginFetch} type="submit" />
 
                 </form>
                 <MyButton name="Регистрация" />
